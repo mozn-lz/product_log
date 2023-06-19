d5bd0c424b71bcb094c11abaca1775cd619dc4fc
mozn-lz <mozn.lozn2000@gmail.com>
2023-06-20 00:34:26 +0200

    sp-tab; save new works

--- public/javascripts/idx.js
+++ public/javascripts/idx.js
@@ -5,18 +5,18 @@ let selectedProduct = {};
 selectedProduct.id = 1 // toto: delete
 
 let resetFields = () => {
-  selectedProduct = null;
-  $('#editProductModalLabel').html('');
-  $('#updateRemainingModalLabel').html('');
-  $('#name').val('');
-  $('#category').val('');
-  $('#shelf').val('');
-  $('#remaining').val('');
-  $('#watch_qty').val('');
-  $('#pack_qty').val('');
-  $('#cost').val('');
-  $('#selling').val('');
-  $('#priority').val('');
+	selectedProduct = null;
+	$('#editProductModalLabel').html('');
+	$('#updateRemainingModalLabel').html('');
+	$('#name').val('');
+	$('#category').val('');
+	$('#shelf').val('');
+	$('#remaining').val('');
+	$('#watch_qty').val('');
+	$('#pack_qty').val('');
+	$('#cost').val('');
+	$('#selling').val('');
+	$('#priority').val('');
 }
 /**
  * 
@@ -25,15 +25,15 @@ let resetFields = () => {
  * @returns SERVER RESPONSE
  */
 let postReq = (path, data) => {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: endpoint + path,
-      type: 'POST',
-      data,
-      success: res => resolve(res),
-      error: e => reject(e)
-    });
-  });
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: endpoint + path,
+			type: 'POST',
+			data,
+			success: res => resolve(res),
+			error: e => reject(e)
+		});
+	});
 }
 /**
  * SELECT PRODUCT
@@ -41,187 +41,191 @@ let postReq = (path, data) => {
  * @param {STRING} id id from 
  */
 let selectProduct = (id) => {
-  console.log(endpoint + '/get-product/', id);
-  postReq('/get-product/', { id })
-    .then(res => {
-      console.log(res);
-      if (res.success) {
-        selectedProduct = res.data;
-        console.log(res.data);
+	console.log(endpoint + '/get-product/', id);
+	postReq('/get-product/', { id })
+		.then(res => {
+			console.log(res);
+			if (res.success) {
+				selectedProduct = res.data;
+				console.log(res.data);
 
-        $('#priceModalLabel').html(res.data.name);
-        $('#editProductModalLabel').html(res.data.name);
-        $('#updateRemainingModalLabel').html(res.data.name);
-        $('#name').val(res.data.name);
-        $('#category').val(res.data.category);
-        $('#shelf').val(res.data.shelf);
-        $('#remaining').val(res.data.remaining);
-        $('#watch_qty').val(res.data.watch_qty);
-        $('#pack_qty').val(res.data.pack_qty);
-        $('#cost').val(res.data.cost);
-        $('#selling').val(res.data.selling);
-        $('#priority').val(res.data.priority);
-        // resetFields()
-      }
-    })
-    .catch(e => {
-      // send error message
-      console.log('434');
-    })
+				$('#priceModalLabel').html(res.data.name);
+				$('#editProductModalLabel').html(res.data.name);
+				$('#updateRemainingModalLabel').html(res.data.name);
+				$('#name').val(res.data.name);
+				$('#category').val(res.data.category);
+				$('#shelf').val(res.data.shelf);
+				$('#remaining').val(res.data.remaining);
+				$('#watch_qty').val(res.data.watch_qty);
+				$('#pack_qty').val(res.data.pack_qty);
+				$('#cost').val(res.data.cost);
+				$('#selling').val(res.data.selling);
+				$('#priority').val(res.data.priority);
+				// resetFields()
+			}
+		})
+		.catch(e => {
+			// send error message
+			console.log('434');
+		})
 }
 /**
  * UPDATE REMAINING
  */
 let onUpdateQty = () => {
-  const remaining = $('#remainingUpdate').val();
+	const remaining = $('#remainingUpdate').val();
 
-  console.warn('onUpdateQty', remaining);
-  console.log('onUpdateQty', remaining)
+	console.warn('onUpdateQty', remaining);
+	console.log('onUpdateQty', remaining)
 
-  if (selectedProduct && selectedProduct.id && Number(remaining)) {
-    postReq('/update-product/', { id: selectedProduct.id, remaining, qty: true })
-      .then(res => {
-        console.log(res);
-        if (res.success) {
-          console.log('OK');
-          // todo: show pop-up success message
-          $('#remainingUpdate').val('');
-          resetFields();
-        } else {
-          console.log('Fail');
-        }
-      })
-      .catch(e => {
-        // todo : 'send error message'
-        console.log('434'); });
-    console.log('good');
-  } else {
-    console.warn('Product load error or remaining quantity error');
-  }
+	if (selectedProduct && selectedProduct.id && Number(remaining)) {
+		postReq('/update-product/', { id: selectedProduct.id, remaining, qty: true })
+			.then(res => {
+				console.log(res);
+				if (res.success) {
+					console.log('OK');
+					// todo: show pop-up success message
+					$('#remainingUpdate').val('');
+					resetFields();
+				} else {
+					console.log('Fail');
+				}
+			})
+			.catch(e => {
+				// todo : 'send error message'
+				console.log('434'); });
+		console.log('good');
+	} else {
+		console.warn('Product load error or remaining quantity error');
+	}
 }
 /**
  * UPDATE PRODUCT INFO
  */
 let onUpdateProduct = () => {
-  let name = $('#name').val().trim();
-  let category = $('#category').val().trim();
-  let shelf = $('#shelf').val().trim();
-  let remaining = $('#remaining').val().trim();
-  let watch_qty = $('#watch_qty').val().trim();
-  let pack_qty = $('#pack_qty').val().trim();
-  let cost = $('#cost').val().trim();
-  let selling = $('#selling').val().trim();
-  let priority = $('#priority').val().trim();
+	let name = $('#name').val().trim();
+	let category = $('#category').val().trim();
+	let shelf = $('#shelf').val().trim();
+	let remaining = $('#remaining').val().trim();
+	let watch_qty = $('#watch_qty').val().trim();
+	let pack_qty = $('#pack_qty').val().trim();
+	let cost = $('#cost').val().trim();
+	let selling = $('#selling').val().trim();
+	let priority = $('#priority').val().trim();
 
-  // todo: validate data
-  if (name.length || category.length || shelf.length ||
-    Number(remaining) || Number(watch_qty) || Number(pack_qty) ||
-    Number(cost) || Number(selling) || Number(priority)) {
-    console.log('update');
+	// todo: validate data
+	if (name.length || category.length || shelf.length ||
+		Number(remaining) || Number(watch_qty) || Number(pack_qty) ||
+		Number(cost) || Number(selling) || Number(priority)) {
+		console.log('update');
 
-    const editProduct = {};
-    validate_name(name)						? editProduct.name			=  name				: '';
-    validate_category(category)		? editProduct.category	=  category		: '';
-    validate_shelf(shelf)			 		? editProduct.shelf			=  shelf			: '';
-    validate_remaining(remaining)	? editProduct.remaining	=  remaining	: '';
-    validate_watch_qty(watch_qty)	? editProduct.watch_qty	=  watch_qty	: '';
-    validate_pack_qty(pack_qty)		? editProduct.pack_qty	=  pack_qty		: '';
-    validate_cost(cost)						? editProduct.cost			=  cost				: '';
-    validate_selling(selling)			? editProduct.selling		=  selling		: '';
-    validate_priority(priority)		? editProduct.priority	=  priority		: '';
+		const editProduct = {};
+		validate_name(name)						? editProduct.name			=  name				: '';
+		validate_category(category)		? editProduct.category	=  category		: '';
+		validate_shelf(shelf)			 		? editProduct.shelf			=  shelf			: '';
+		validate_remaining(remaining)	? editProduct.remaining	=  remaining	: '';
+		validate_watch_qty(watch_qty)	? editProduct.watch_qty	=  watch_qty	: '';
+		validate_pack_qty(pack_qty)		? editProduct.pack_qty	=  pack_qty		: '';
+		validate_cost(cost)						? editProduct.cost			=  cost				: '';
+		validate_selling(selling)			? editProduct.selling		=  selling		: '';
+		validate_priority(priority)		? editProduct.priority	=  priority		: '';
 
-    // send request
-    postReq('/update-product/', { id: selectedProduct.id, remaining, edit: true })
-      .then(res => {
-        // todo send success message to user 
-        resetFields();
-      })
-      .catch(e => {
-        // todo: error message to user
-      });
-  } else {
-    // if validation fails
-    // todo: error message to user
-  }
+		// send request
+		postReq('/update-product/', { id: selectedProduct.id, remaining, edit: true })
+			.then(res => {
+				// todo send success message to user 
+				resetFields();
+			})
+			.catch(e => {
+				// todo: error message to user
+			});
+	} else {
+		// if validation fails
+		// todo: error message to user
+	}
 }
 /**
  * ADD NEW PRODUCT
  */
 let onSaveNewProduct = () => {
-  const product = {
-    name			: $('#Name').val().trim(),
-    category	: $('#Category').val().trim(),
-    shelf			: $('#Shelf').val().trim(),
-    remaining	: $('#Remaining').val().trim(),
-    watch_qty	: $('#Watch_qty').val().trim(),
-    pack_qty	: $('#Pack_qty').val().trim(),
-    cost			: $('#Cost').val().trim(),
-    selling		: $('#Selling').val().trim(),
-    priority	: $('#Priority').val().trim(),
-  }
+	const product = {
+		name			: $('#Name').val().trim(),
+		category	: $('#Category').val().trim(),
+		shelf			: $('#Shelf').val().trim(),
+		remaining	: $('#Remaining').val().trim(),
+		watch_qty	: $('#Watch_qty').val().trim(),
+		pack_qty	: $('#Pack_qty').val().trim(),
+		cost			: $('#Cost').val().trim(),
+		selling		: $('#Selling').val().trim(),
+		priority	: $('#Priority').val().trim(),
+		itemCode  : $('#itemCode').val().trim()
+	}
 
-  // todo: validate data
-  if (product.name.length || product.category.length || product.shelf.length ||
-    Number(product.remaining) || Number(product.watch_qty) || Number(product.pack_qty) ||
-    Number(product.cost) || Number(product.selling) || Number(product.priority)) {
-    console.log('qas');
-      postReq('/update-product/', { product, newProduct: true })
-        .then( res => {
-          console.log(res);
-          // todo send success message to user 
-              // product saved
-          // resetFields();
-            // $('#Name').val('');
-            // $('#Category').val('');
-            // $('#Shelf').val('');
-            // $('#Remaining').val('');
-            // $('#Watch_qty').val('');
-            // $('#Pack_qty').val('');
-            // $('#Cost').val('');
-            // $('#Selling').val('');
-            // $('#Priority').val('');
-        })
-        .catch(e => {
-          // todo: error message to user
-          console.log(e);
-        });
-  } else {
-    // if validation fails
-    // todo: error message to user
-    console.log('Name: ', product.name.length)
-    console.log('Category: ', product.category.length)
-    console.log('Shelf: ', product.shelf.length)
-    console.log('Remaining: ', Number(product.remaining))
-    console.log('Watch_qty: ', Number(product.watch_qty))
-    console.log('Pack_qty: ', Number(product.pack_qty))
-    console.log('Cost: ', Number(product.cost))
-    console.log('Selling: ', Number(product.selling))
-    console.log('Priority: ', Number(product.priority))
+	// todo: validate data
+	if (product.name.length || product.category.length || product.shelf.length ||
+		Number(product.remaining) || Number(product.watch_qty) || Number(product.pack_qty) ||
+		Number(product.cost) || Number(product.selling) || Number(product.priority)) {
+		console.log('qas');
+		postReq('/update-product/', { product, newProduct: true })
+			.then( res => {
+				console.log(res);
+				// todo send success message to user 
+				// product saved
+				// resetFields();
+					$('#Name').val('');
+					$('#Category').val('');
+					$('#Shelf').val('');
+					$('#Remaining').val('');
+					$('#Watch_qty').val('');
+					$('#Pack_qty').val('');
+					$('#Cost').val('');
+					$('#Selling').val('');
+					$('#Priority').val('');
+					$('#itemCode').val('');
+					$("#newProductModalClose").click();
+			})
+			.catch(e => {
+				// todo: error message to user
+				console.log(e);
+			});
+	} else {
+		// if validation fails
+		// todo: error message to user
+		console.log('Name: ', product.name.length)
+		console.log('Category: ', product.category.length)
+		console.log('Shelf: ', product.shelf.length)
+		console.log('Remaining: ', Number(product.remaining))
+		console.log('Watch_qty: ', Number(product.watch_qty))
+		console.log('Pack_qty: ', Number(product.pack_qty))
+		console.log('Cost: ', Number(product.cost))
+		console.log('Selling: ', Number(product.selling))
+		console.log('Priority: ', Number(product.priority))
 
-    console.warn('invalid data');
-  }
+		console.warn('invalid data');
+		$("#newProductModalClose").click();
+	}
 }
 /**
  * UPDATE PRICES
  */
 let onPriceUpdate = () => {
-  const cost = $('#product_cost');
-  const selling = $('#product_selling');
+	const cost = $('#product_cost');
+	const selling = $('#product_selling');
 
-  // validate cost & selling
-  postReq('/update-product/', { id: selectedProduct.id, selling, cost, priceUpdate: true })
-    .then(res => {
-      if (res.success) {
-        // send success message >>> product qty updated
-      } else {
-        // server error saving product
-      }
-    })
-    .catch(e => {
-      // error from server
-    });
+	// validate cost & selling
+	postReq('/update-product/', { id: selectedProduct.id, selling, cost, priceUpdate: true })
+		.then(res => {
+			if (res.success) {
+				// send success message >>> product qty updated
+			} else {
+				// server error saving product
+			}
+		})
+		.catch(e => {
+			// error from server
+		});
 
-  // reset fields
-  cost.val('');
-  selling.val('');
+	// reset fields
+	cost.val('');
+	selling.val('');
 }
