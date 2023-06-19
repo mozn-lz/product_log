6f9e473f2075fa6fa68514dbfc1ee042754a04dc
mozn-lz <mozn.lozn2000@gmail.com>
2023-06-20 00:31:22 +0200

    env server name

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Connect to database
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-	host: 'localhost',
+	host: process.env.DB_HOST,
 	dialect: 'mariadb'
 });
 
